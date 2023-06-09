1f5549015e1476133eb70917a86d82c0cd05474f
Wambui-N <wambuin18@gmail.com>
2023-06-09 09:30:51 +0300

    commit

--- resources/views/admin/assets/overview.blade.php
+++ resources/views/admin/assets/overview.blade.php
@@ -4,10 +4,10 @@
     <section class="content">
         <div class="container-fluid">
             <!-- Info boxes -->
-            <div class="row">
+            <div class="d-flex flex-row pt-3">
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box">
-                        <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+                    <div class="info-box bg-light">
+                        <span class="info-box-icon bg-dark"><i class="fas fa-cog"></i></span>
 
                         <div class="info-box-content">
                             <span class="info-box-text">CPU Traffic</span>
@@ -22,8 +22,8 @@
                 </div>
                 <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+                    <div class="info-box bg-light">
+                        <span class="info-box-icon bg-dark"><i class="fas fa-thumbs-up"></i></span>
 
                         <div class="info-box-content">
                             <span class="info-box-text">Likes</span>
@@ -39,8 +39,8 @@
                 <div class="clearfix hidden-md-up"></div>
 
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+                    <div class="info-box bg-light">
+                        <span class="info-box-icon bg-dark"><i class="fas fa-shopping-cart"></i></span>
 
                         <div class="info-box-content">
                             <span class="info-box-text">Sales</span>
@@ -52,8 +52,8 @@
                 </div>
                 <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+                    <div class="info-box bg-light">
+                        <span class="info-box-icon bg-dark"><i class="fas fa-users"></i></span>
 
                         <div class="info-box-content">
                             <span class="info-box-text">New Members</span>
--- resources/views/partial/menu.blade.php
+++ resources/views/partial/menu.blade.php
@@ -38,13 +38,11 @@
 
 
 <!--Student Dashboard-->
-
-
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
 
         <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <nav class="main-header navbar navbar-expand bg-light border-bottom">
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -90,7 +88,7 @@
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <aside class="main-sidebar bg-light border-end">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
                 <img src={{ asset('dist/img/AdminLTELogo.png') }} alt="AdminLTE Logo"
